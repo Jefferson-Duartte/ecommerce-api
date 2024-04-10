ac59eff471990d91657dff4e96a333fc6a9921e5
Jefferson Duarte <duartejefferson123@gmail.com>
2024-04-10 09:54:30 -0300

    Create service layer and component registration

--- src/main/java/com/jefferson/ecommerce/resources/UserResource.java
+++ src/main/java/com/jefferson/ecommerce/resources/UserResource.java
@@ -1,22 +1,37 @@
 package com.jefferson.ecommerce.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jefferson.ecommerce.entities.User;
+import com.jefferson.ecommerce.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<User> findAll() {
+	public ResponseEntity<List<User>> findAll() {
 		
-		User u = new User(1L, "Jefferson", "duartejefferson123@gmail.com", "484845", "556252");
+		List<User> allUsers = userService.findAll();
 		
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(allUsers);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
